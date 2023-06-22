82479f15cc3ee52f4005b3074afb64845a5a6e70
Prabhu-Manchineella_bcgovent <prabhu.manchineella@gov.bc.ca>
2023-06-22 13:08:20 -0400

    update policy

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,18 +1,15 @@
 provider "aws" {
   region = var.aws_region
 }
-
 resource "random_pet" "DB_NAME" {
   prefix = "ssp-greetings"
   length = 2
 }
-
 /* Dynamo DB Table */
 resource "aws_dynamodb_table" "ssp-greetings" {
   name      = random_pet.DB_NAME.id
   hash_key  = "pid"
   range_key = "createdAt"
-
   # billing_mode   = "PAY_PER_REQUEST"
   read_capacity  = 20
   write_capacity = 20
@@ -20,7 +17,6 @@ resource "aws_dynamodb_table" "ssp-greetings" {
     name = "pid"
     type = "S"
   }
-
   attribute {
     name = "createdAt"
     type = "S"
@@ -34,12 +30,10 @@ resource "random_pet" "upload_bucket_name" {
   prefix = "upload-bucket"
   length = 2
 }
-
 resource "aws_s3_bucket" "upload_bucket" {
   bucket        = random_pet.upload_bucket_name.id
   force_destroy = true
 }
-
 # Redirect all traffic from the ALB to the target group
 data "aws_alb_listener" "front_end" {
   load_balancer_arn = data.aws_lb.load_balancer.arn
@@ -49,7 +43,6 @@ resource "random_pet" "target_group_name" {
   prefix = "ssp"
   length = 2
 }
-
 resource "aws_alb_target_group" "app" {
   name                 = random_pet.target_group_name.id
   port                 = var.app_port
@@ -57,7 +50,6 @@ resource "aws_alb_target_group" "app" {
   vpc_id               = module.network.aws_vpc.id
   target_type          = "instance"
   deregistration_delay = 30
-
   health_check {
     healthy_threshold   = "2"
     interval            = "5"
@@ -67,27 +59,19 @@ resource "aws_alb_target_group" "app" {
     path                = var.health_check_path
     unhealthy_threshold = "2"
   }
-
-
 }
-
 resource "aws_lb_listener_rule" "host_based_weighted_routing" {
   listener_arn = data.aws_alb_listener.front_end.arn
-
   action {
     type             = "forward"
     target_group_arn = aws_alb_target_group.app.arn
   }
-
   condition {
     host_header {
       values = [for sn in var.service_names : "${sn}.*"]
     }
   }
 }
-
-
-
 data "template_file" "userdata_script" {
   template = file("userdata.tpl")
   vars = {
@@ -96,56 +80,59 @@ data "template_file" "userdata_script" {
     bucketName = aws_s3_bucket.upload_bucket.id
     DB_NAME    = aws_dynamodb_table.ssp-greetings.id
     branch     = var.branch
-
   }
 }
-
 resource "random_pet" "instances_name" {
   prefix = "ssp"
   length = 2
 }
-
-
 /* Auto Scaling & Launch Configuration */
 module "asg" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "5.0.0"
-
+  version = "6.10.0"
   name = random_pet.instances_name.id
-
   # Launch configuration creation
-  lc_name                   = var.lc_name
+  launch_template_name      = var.lc_name
   image_id                  = var.iamge_id
   instance_type             = "t2.micro"
-  spot_price                = "0.0038"
-  security_groups           = [module.network.aws_security_groups.app.id]
-  iam_instance_profile_name = random_pet.instances_name.id
-  user_data                 = data.template_file.userdata_script.rendered
-  use_lc                    = true
-  create_lc                 = true
-
-
-
-
-
-  root_block_device = [
-    {
-      volume_size = "50"
-      volume_type = "gp2"
-    },
-  ]
-
-
-  # Auto scaling group creation
   vpc_zone_identifier       = module.network.aws_subnet_ids.app.ids
+  security_groups           = [module.network.aws_security_groups.app.id]
+  user_data                 = base64encode(data.template_file.userdata_script.rendered)
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+      }, {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 30
+        volume_type           = "gp2"
+      }
+    }
+  ]
+  instance_market_options = {
+    market_type = "spot"
+  }
+  # Auto scaling group creation
+  # vpc_zone_identifier       = module.network.aws_subnet_ids.app.ids
   health_check_type         = "EC2"
   min_size                  = 1
   max_size                  = 1
+  iam_instance_profile_arn = aws_iam_instance_profile.ssp_profile.arn
   desired_capacity          = 1
   wait_for_capacity_timeout = 0
   health_check_grace_period = 500
   target_group_arns         = [aws_alb_target_group.app.arn]
-
   instance_refresh = {
     strategy = "Rolling"
     preferences = {
@@ -153,14 +140,11 @@ module "asg" {
     }
     triggers = ["tag"]
   }
-
 }
-
 resource "aws_iam_instance_profile" "ssp_profile" {
   name = random_pet.instances_name.id
   role = aws_iam_role.ssp-db.name
 }
-
 resource "aws_iam_role" "ssp-db" {
   name               = random_pet.instances_name.id
   assume_role_policy = <<EOF
@@ -178,13 +162,9 @@ resource "aws_iam_role" "ssp-db" {
 }
 EOF
 }
-
 resource "aws_iam_policy" "db_ssp" {
   name = random_pet.instances_name.id
-
   description = "policy to give dybamodb permissions to ec2"
-
-
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -197,17 +177,21 @@ resource "aws_iam_policy" "db_ssp" {
           "dynamodb:GetItem",
           "dynamodb:Query",
           "dynamodb:UpdateItem",
-          "dynamodb:UpdateTable"
+          "dynamodb:UpdateTable",
+          "iam:GetRole",
+          "iam:PassRole",
+          "ec2:RunInstances",
+          "ec2:CreateTags"
         ],
         "Resource" : "*"
       },
-
       {
         "Action" : [
           "kms:DescribeKey",
           "kms:GenerateDataKey*",
           "kms:Decrypt",
           "kms:Encrypt",
+          "kms:CreateGrant",
           "kms:ReEncrypt*"
         ],
         "Resource" : "*",
@@ -240,6 +224,8 @@ resource "aws_iam_policy" "db_ssp" {
           "cloudwatch:PutMetricData",
           "ec2:DescribeVolumes",
           "ec2:DescribeTags",
+          "ec2:GetEbsEncryptionByDefault",
+				  "ec2:EnableEbsEncryptionByDefault",
           "logs:PutLogEvents",
           "logs:DescribeLogStreams",
           "logs:DescribeLogGroups",
@@ -306,13 +292,10 @@ resource "aws_iam_policy" "db_ssp" {
         ],
         "Resource" : "*"
       }
-
-
     ]
   })
 }
 resource "aws_iam_role_policy_attachment" "test-attach" {
   role       = aws_iam_role.ssp-db.name
   policy_arn = aws_iam_policy.db_ssp.arn
-
 }
